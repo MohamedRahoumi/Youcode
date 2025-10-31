d12a43bc9958db78e403bebf61240ba18ba637d1
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-10-31 21:25:21 +0100

    declare le variable pour travail

---
+++ Sprint2/Breif1/TypingGame/main.js
@@ -0,0 +1,9 @@
+const phrase = "La programmation est un art qui combine la logique et la creativiter Chaque ligne de code ecrite represente une idee transformee en action Pour devenir un bon programmeur il faut de la patience de la pratique";
+const textEl = document.querySelector(".text");
+const inputEl = document.getElementById("input");
+const timeEl = document.querySelector(".time");
+const wpmEl = document.querySelector(".wpm");
+let timer = null;
+let timeLeft = 60;
+let started = false;
+
